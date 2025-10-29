d69fb5d036a08e8ca06def4ade0e96def8160f7b
tympush <tymur.pushnoy@gmail.com>
2025-10-29 16:16:03 +0100

    finished checklist in 1 file

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,5 +1,72 @@
 import "./styles.css"
+import { useState } from "react"
 
 export default function App() {
-  return "Hiiii :3"
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title: newItem, completed: 
+        false }
+      ]
+    })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return(
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+            value={newItem} 
+            onChange={e => setNewItem(e.target.value)} 
+            type="text" 
+            id="item"
+          />
+          <button className="btn">Add</button>
+        </div>
+        <h1 className="header">Todo List</h1>
+        <ul className="list">
+          {todos.length === 0 && "No Todos"}
+          {todos.map(todo => {
+            return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed}
+                onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+                {todo.title}
+                <button onClick={() => deleteTodo(todo.id)} 
+                className="btn btn-danger">Delete</button>
+              </label>
+            </li>
+            )
+          })}
+        </ul>
+      </form>
+    </>
+  )
 }
